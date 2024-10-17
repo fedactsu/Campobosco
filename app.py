ae6ba76933a8f79285adb67291764efd418428da
fedactsu <106325142+fedactsu@users.noreply.github.com>
2024-10-17 18:48:54 -0300

    up

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ from datetime import timedelta
 import datetime as dt
 import locale
 import zipfile
-import bcrypt
 import os
 
 app=Flask(__name__)
@@ -20,10 +19,27 @@ application=app
 def index():
     return render_template('index.html')
 
+@app.route("/inicio")
+def inicio():
+    return render_template('estructura/inicio.html')
+#-----------------------------------------------------------------------------------------------MIERCOLES
+@app.route('/miercoles')
+def miercoles():
+    return render_template('1_miercoles/1_miercoles.html')
+
+@app.route('/miercoles/laudes')
+def miercoles_laudes():
+    return render_template('1_miercoles/2_mier_laudes.html')
+
+@app.route('/miercoles/misa')
+def miercoles_misa():
+    return render_template('1_miercoles/3_mier_misa.html')
+
+
 #-----------------------------------------------------------------------------------------------ERRORES
 @app.errorhandler(404)
 def page_not_found(error):
-    return render_template('view/errores/404.html')
+    return render_template('estructura/404.html')
 
 
 

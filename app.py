ffb1c523e84074f05db221d23158b4ed2ac6ea9d
fedactsu <106325142+fedactsu@users.noreply.github.com>
2025-06-22 21:59:46 -0400

    kk

--- app.py
+++ app.py
@@ -28,6 +28,17 @@ def obtenerdias():
 def obteneroracion():
     return ['laudes','vísperas','completas']
 
+def obtenercoso():
+    return [
+        ["Domingo 29 de junio", "San Pablo y San Pedro apóstoles", "1_DOMINGO/1_DOMINGO.html"],
+        ["Lunes 30 de junio", "Decimotercera semana del Tiempo Ordinario", "2_LUNES/2_LUNES.html"],
+        ["Martes 01 de julio", "Decimotercera semana del Tiempo Ordinario", "3_MARTES/3_MARTES.html"],
+        ["Miércoles 02 de julio", "Decimotercera semana del Tiempo Ordinario", "4_MIERCOLES/4_MIERCOLES.html"],
+        ["Jueves 03 de julio", "Decimotercera semana del Tiempo Ordinario - Santo Tomás apóstol", "5_JUEVES/5_JUEVES.html"],
+        ["Viernes 04 de julio", "Decimotercera semana del Tiempo Ordinario", "6_VIERNES/6_VIERNES.html"],
+        ["Sábado 05 de julio", "Decimotercera semana del Tiempo Ordinario", "7_SABADO/7_SABADO.html"]
+    ]
+
 @app.route("/inicio")
 def inicio():
     return render_template('estructura/inicio.html',dia=obtenerdias())
@@ -37,33 +48,31 @@ def base():
 #-----------------------------------------------------------------------------------------------DOMINGO
 @app.route('/domingo')
 def domingo():
-    dia="Domingo 29"
-    onomastico = "San Pablo y San Pedro apóstoles"
-    return render_template('1_DOMINGO/1_DOMINGO.html',domingo=obteneroracion(),dia=dia,onomastico=onomastico)
+    return render_template(obtenercoso()[0][2],domingo=obteneroracion(),dia=obtenercoso()[0][0],onomastico=obtenercoso()[0][1])
 #-----------------------------------------------------------------------------------------------LUNES
 @app.route('/lunes')
 def lunes():
-    return render_template('2_LUNES/2_LUNES.html')
+    return render_template(obtenercoso()[1][2],domingo=obteneroracion(),dia=obtenercoso()[1][0],onomastico=obtenercoso()[1][1])
 #-----------------------------------------------------------------------------------------------MARTES
 @app.route('/martes')
 def martes():
-    return render_template('3_MARTES/3_MARTES.html')
+    return render_template(obtenercoso()[2][2],domingo=obteneroracion(),dia=obtenercoso()[2][0],onomastico=obtenercoso()[2][1])
 #-----------------------------------------------------------------------------------------------MIÉRCOLES
 @app.route('/miercoles')
 def miercoles():
-    return render_template('4_MIERCOLES/4_MIERCOLES.html')
+    return render_template(obtenercoso()[3][2],domingo=obteneroracion(),dia=obtenercoso()[3][0],onomastico=obtenercoso()[3][1])
 #-----------------------------------------------------------------------------------------------JUEVES
 @app.route('/jueves')
 def jueves():
-    return render_template('5_JUEVES/5_JUEVES.html')
+    return render_template(obtenercoso()[4][2],domingo=obteneroracion(),dia=obtenercoso()[4][0],onomastico=obtenercoso()[4][1])
 #-----------------------------------------------------------------------------------------------VIERNES
 @app.route('/viernes')
 def viernes():
-    return render_template('6_VIERNES/6_VIERNES.html')
+    return render_template(obtenercoso()[5][2],domingo=obteneroracion(),dia=obtenercoso()[5][0],onomastico=obtenercoso()[5][1])
 #-----------------------------------------------------------------------------------------------SABADO
 @app.route('/sabado')
 def sabado():
-    return render_template('7_SABADO/7_SABADO.html')
+    return render_template(obtenercoso()[6][2],domingo=obteneroracion(),dia=obtenercoso()[6][0],onomastico=obtenercoso()[6][1])
 #-----------------------------------------------------------------------------------------------ERRORES
 @app.errorhandler(404)
 def page_not_found(error):
